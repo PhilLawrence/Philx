b2bda1d79a18e7083fa55bbf9597aac99558082b
Phil Lawrence <pwlawrence@hotmail.com>
2018-10-22 13:27:35 +0100

    Playing with OData

---
+++ ODataService/App_Start/WebApiConfig.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using ODataService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace ODataService
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+
+			var builder = new ODataConventionModelBuilder();
+
+			builder.EntitySet<Product>("Products");
+
+			config.MapODataServiceRoute("ODataRoute", null, builder.GetEdmModel());
+
+
+			//ODataModelBuilder builder = new ODataConventionModelBuilder();
+			//builder.EntitySet<Product>("Products");
+			//config.MapODataServiceRoute(
+			//	routeName: "ODataRoute",
+			//	routePrefix: null,
+			//	model: builder.GetEdmModel());
+
+
+
+		}
+	}
+}
---
+++ ProductsApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsApp
+{
+	class Program
+	{
+
+		// Get an entire entity set.
+		static void ListAllProducts(Default.Container container)
+		{
+			
+			foreach (var p in container.Products)
+			{
+				Console.WriteLine("{0} {1}", p.Name, p.ID);
+			}
+		}
+
+		static void AddProduct(Default.Container container, ODataService.Models.Product product)
+		{
+			container.AddToProducts(product);
+			var serviceResponse = container.SaveChanges();
+			foreach (var operationResponse in serviceResponse)
+			{
+				Console.WriteLine("Response: {0}", operationResponse.StatusCode);
+			}
+		}
+
+		static void Main(string[] args)
+		{
+
+			// TODO: Replace with your local URI.
+			string serviceUri = "http://localhost:57191/";
+			var container = new Default.Container(new Uri(serviceUri));
+
+			var product = new ODataService.Models.Product()
+			{
+				Name = "Dave",
+				ID = 12
+			};
+
+			AddProduct(container, product);
+
+			ListAllProducts(container);
+
+			Console.WriteLine("Done");
+
+			Console.ReadLine();
+		}
+	}
+}
